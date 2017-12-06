ee725de72b651d04f4a0f082130fcf209e7c47f3
Valeria Perticara <vperticara@it.ibm.com>
2017-12-06 16:42:19 +0100

    Create cf-deployment pipeline
    [#153280888]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- ci/scripts/deploy-cf/task.sh
+++ ci/scripts/deploy-cf/task.sh
@@ -9,6 +9,9 @@ preflight_check() {
   test -n "${BOSH_CA_CERT}"
   test -n "${CF_DEPLOYMENT}"
   test -n "${API_PASSWORD}"
+  test -n "${S3_ACCESS_KEY}"
+  test -n "${S3_SECRET_KEY}"
+  test -n "${S3_HOST}"
   set -x
 }
 
@@ -17,6 +20,8 @@ function main(){
   preflight_check
 
   export BOSH_ENVIRONMENT="https://${BOSH_DIRECTOR}:25555"
+
+  sed -i -e "s/region: ((aws_region))/host: ((blobstore_s3_host))/g" "${root}/cf-deployment/operations/use-s3-blobstore.yml"
 
   bosh interpolate "${root}/cf-deployment/cf-deployment.yml" \
     --vars-store "${root}/variables_output.yml" \
@@ -28,6 +33,15 @@ function main(){
     -o "${root}/cf-deployment/operations/scale-to-one-az.yml" \
     -o "${root}/cf-deployment/operations/use-latest-stemcell.yml" \
     -o "${root}/postgres-release/ci/templates/use-latest-postgres-release.yml" \
+    -v blobstore_access_key_id="${S3_ACCESS_KEY}" \
+    -v blobstore_secret_access_key="${S3_SECRET_KEY}" \
+    -v blobstore_s3_host="${S3_HOST}" \
+    -o "${root}/cf-deployment/operations/templates/use-s3-blobstore.yml" \
+    -v buildpack_directory_key="${CF_DEPLOYMENT}-cc-buildpacks" \
+    -v droplet_directory_key="${CF_DEPLOYMENT}-cc-droplets" \
+    -v app_package_directory_key="${CF_DEPLOYMENT}-cc-app-package" \
+    -v resource_directory_key="${CF_DEPLOYMENT}-cc-resource" \
+    -o "${root}/postgres-release/ci/templates/change_uaa_props.yml" \
     > "${root}/pgci_cf.yml"
 
   bosh --non-interactive --deployment="${CF_DEPLOYMENT}" deploy "${root}/pgci_cf.yml"
